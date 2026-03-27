136ab29b97a1ad7aac7e7b8dee7af8d7270e6c3e
nikyezhovv <nik.yezhovv@gmail.com>
2026-03-27 23:29:30 +0500

    feat: add Entity base class and User domain model

---
+++ Tasteory/Tasteory.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities;
+
+public class User : Entity
+{
+    public string UserName { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
+
+    public User(Guid id, string userName, string email, string passwordHash) : base(id)
+    {
+        UserName = userName;
+        Email = email;
+        PasswordHash = passwordHash;
+    }
+}
