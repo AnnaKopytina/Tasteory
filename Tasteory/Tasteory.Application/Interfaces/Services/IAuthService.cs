426b594116fc380ee0e945d7b38d0a683bae15f9
nikyezhovv <nik.yezhovv@gmail.com>
2026-03-28 00:53:05 +0500

    feat: add AuthService for registration and loign

---
+++ Tasteory/Tasteory.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Interfaces.Services;
+
+public interface IAuthService
+{
+    public Task RegisterAsync(string userName, string email, string password);
+    public Task<string> LoginAsync(string email, string password);
+}
---
+++ Tasteory/Tasteory.Application/Services/AuthService.cs
@@ -0,0 +1,56 @@
+﻿using Application.Interfaces.Auth;
+using Application.Interfaces.Services;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly IJwtProvider _jwtProvider;
+
+    public AuthService(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtProvider jwtProvider)
+    {
+        _userRepository = userRepository;
+        _passwordHasher = passwordHasher;
+        _jwtProvider = jwtProvider;
+    }
+
+    public async Task RegisterAsync(string userName, string email, string password)
+    {
+        var existingUser = await _userRepository.GetByEmailAsync(email);
+
+        if (existingUser is not null)
+        {
+            //TODO: кастомные эксепшены забабахать
+            throw new ArgumentException("User with the same Email already exists");
+        }
+
+        var passwordHash = _passwordHasher.Generate(password);
+
+        var user = new User(Guid.NewGuid(), userName, email, passwordHash);
+
+        await _userRepository.AddAsync(user);
+    }
+
+    public async Task<string> LoginAsync(string email, string password)
+    {
+        var user = await _userRepository.GetByEmailAsync(email);
+
+        if (user is null)
+        {
+            throw new UnauthorizedAccessException("Invalid Email or Password");
+        }
+
+        var isValidPassword = _passwordHasher.Verify(password, user.PasswordHash);
+
+        if (!isValidPassword)
+        {
+            throw new UnauthorizedAccessException("Invalid Email or Password");
+        }
+
+        return _jwtProvider.Generate(user);
+    }
+}
