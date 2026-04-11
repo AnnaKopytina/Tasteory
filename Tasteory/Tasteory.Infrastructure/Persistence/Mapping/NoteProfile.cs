8453b570651fe804e10eb13cbd9fa89dc8b23e47
nikyezhovv <nik.yezhovv@gmail.com>
2026-04-12 03:32:34 +0500

    feat: add note mapping profile, implement NoteRepository

---
+++ Tasteory/Tasteory.Infrastructure/Persistence/Mapping/NoteProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Persistence.Entities;
+
+namespace Infrastructure.Persistence.Mapping;
+
+public class NoteProfile : Profile
+{
+    public NoteProfile()
+    {
+        CreateMap<StepNoteEntity, StepNote>()
+            .ForCtorParam("text", opt => opt.MapFrom(src => src.NoteText));
+
+        CreateMap<StepNote, StepNoteEntity>()
+            .ForMember(dest => dest.NoteText, opt => opt.MapFrom(src => src.Text));
+    }
+}
