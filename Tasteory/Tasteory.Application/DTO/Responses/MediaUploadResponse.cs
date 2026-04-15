cec06af62b8a3994ec61f6fefaacf34413044a88
nyezhovv <nik.yezhovv@gmail.com>
2026-04-15 21:25:15 +0500

    add media upload service with storage interface

---
+++ Tasteory/Tasteory.Application/DTO/Responses/MediaUploadResponse.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+
+namespace Application.DTO.Responses;
+
+public class MediaUploadResponse
+{
+    public string Url { get; set; } = string.Empty;
+    public MediaType MediaType { get; set; }
+    public long SizeBytes { get; set; }
+}
