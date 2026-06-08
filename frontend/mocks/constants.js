03ef19577ef0a51d3a858f21a8ba3b2ac6d4986a
nyezhovv <nik.yezhovv@gmail.com>
2026-06-08 21:19:26 +0500

    remove unused mocks

--- frontend/mocks/constants.js
+++
@@ -1,8 +0,0 @@
-export const PROFILE_USER_ID = 'user-galina';
-
-export const MAIN_FEED_RECIPE_IDS = [
-    'main-buckwheat-avocado-bowl',
-    'main-baked-cod-vegetables',
-    'main-banana-pancakes',
-    'main-mushroom-soup'
-];
--- frontend/services/mock-data.js
+++
@@ -1,6 +0,0 @@
-export {PROFILE_USER_ID} from '../mocks/constants.js';
-export {users} from '../mocks/fixtures/users.js';
-export {recipes} from '../mocks/fixtures/recipes.js';
-export {groups} from '../mocks/fixtures/groups.js';
-export {createMockStore, deepClone} from '../mocks/store/create-mock-store.js';
-export {createHandlers as createMockHandlers} from '../mocks/handlers/index.js';
